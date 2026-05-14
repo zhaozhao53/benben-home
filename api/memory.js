1c3478ccd50bf960d1e68722fac2a126cb4a6dda
zhaozhao53 <mei403243@proton.me>
2026-05-15 00:30:18 +0800

    Remove unnecessary whitespace in memory.js

--- api/memory.js
+++ api/memory.js
@@ -1,4 +1,4 @@
-  export default async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
